82568fd374015fb96237c2064c22c613524738d0
ramin00shlbiy <raminayobi001@gmail.com>
2025-09-15 11:18:57 +0430

    data.js

---
+++ data.js
@@ -0,0 +1,31 @@
+// لیست محصولات
+const allProducts = [
+    {
+        id: 1,
+        name: "نوشابه کوکاکولا",
+        price: 50,
+        image: "https://via.placeholder.com/300x200?text=نوشابه+کوکاکولا",
+        category: "drinks"
+    },
+    {
+        id: 2,
+        name: "چیپس سیب‌زمینی",
+        price: 40,
+        image: "https://via.placeholder.com/300x200?text=چیپس+سیب‌زمینی",
+        category: "snacks"
+    },
+    {
+        id: 3,
+        name: "شامپو سر",
+        price: 120,
+        image: "https://via.placeholder.com/300x200?text=شامپو+سر",
+        category: "hygiene"
+    },
+    {
+        id: 4,
+        name: "شیرینی شکلاتی",
+        price: 80,
+        image: "https://via.placeholder.com/300x200?text=شیرینی+شکلاتی",
+        category: "foods"
+    }
+];
